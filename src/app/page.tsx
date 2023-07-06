200ec615502183fe44c6ca0f60be793edb9c6b18
Vitor Santos <pireshugo737@gmail.com>
2023-07-05 21:59:07 -0300

    :sparkles: Feat: search trip
    add initial search trip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import { useSession } from 'next-auth/react';
+import TripSerach from './components/TripSearch';
 
 export default function Home() {
   return (
     <div>
-
+      <TripSerach />
     </div>
   );
 }
