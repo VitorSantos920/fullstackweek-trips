95b4c37214b1abd03698d03e139284e3609d8292
Vitor Santos <pireshugo737@gmail.com>
2023-07-06 01:40:35 -0300

    :sparkles: Feat: recomended trips
    add recommended trips component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@
 
 import { useSession } from 'next-auth/react';
 import TripSerach from './components/TripSearch';
-import QuickSearch from './about/components/QuickSearch';
+import QuickSearch from './components/QuickSearch';
+import RecommendedTrips from './components/RecommendedTrips';
 
 export default function Home() {
   return (
     <div>
       <TripSerach />
       <QuickSearch />
+      <RecommendedTrips />
     </div>
   );
 }
